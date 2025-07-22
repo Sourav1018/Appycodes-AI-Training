9c7a85693908cbba3d464bc304c728b646531d3e
Cristiano Ronaldo <sourav.ghosh@appycodes.com>
2025-07-22 16:15:39 +0530

    add: adjust code to feed data to model for generate LLM response.

--- app/services/openai_chat.py
+++ app/services/openai_chat.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+
 from app.utils.openai_tools import calculator_tool
 
 # Load environment variables from .env file
@@ -8,13 +9,21 @@ load_dotenv()
 # Create OpenAI client using the environment variable
 client = OpenAI()
 
-def ask_openai(message: str):
+
+def ask_openai(message):
+    if not isinstance(message, (str, list)):
+        raise TypeError("messages must be a string or a list of message dicts")
+
+    # if isinstance(message, list):
+    #     for msg in message:
+    #         if not isinstance(msg, dict) or "role" not in msg:
+    #             raise ValueError(
+    #                 "Each message in the list must be a dict with 'role' and 'content'"
+    #             )
     response = client.responses.create(
         model="gpt-3.5-turbo",
         instructions="You are a helpful assistant.",
-        input=[
-            {"role": "user", "content": message}
-        ],
+        input=message,
         tools=[calculator_tool],
     )
     return response
