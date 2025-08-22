75c127625bda53ec607c620a60d559726e07cb99
Cristiano Ronaldo <sourav.ghosh@appycodes.com>
2025-08-22 15:33:21 +0530

    add: the app config and .env.example update

---
+++ app/config/app_config.py
@@ -0,0 +1,21 @@
+from pydantic import IPvAnyAddress
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    SUPABASE_URL: str
+    SUPABASE_KEY: str
+    USE_SUPABASE: bool = False
+    HOST: IPvAnyAddress
+    PORT: int
+    RELOAD: bool = False
+    OPENAI_API_KEY: str
+    QDRANT_API_KEY: str
+    QDRANT_CLUSTER_URL: str
+
+    class Config:
+        env_file = ".env"  # load from .env automatically
+
+
+# Create a global settings object
+settings = Settings()
